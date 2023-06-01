cbbe8abae2cf4b70964ce5bcbeb91c3d1a04d708
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-06-01 22:58:18 +0100

    added a public gameobject to control the inventory canvas to active true or false when the player presses i and the playercontroller assigns a bool to true or false

--- Inventory/GameInventory.cs
+++ Inventory/GameInventory.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class GameInventory : MonoBehaviour
 {
 
-    
+    public GameObject inventoryUI;
+    private PlayerController playerController;
 
     public GameObject coffeePanel;
+
+    private void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if(playerController.inventoryOpen == true)
+        {
+            inventoryUI.SetActive(true);
+        }
+        else
+        {
+            inventoryUI.SetActive(false);
+        }
+    }
 
     public void OnCoffeeButtonClick()
     {
