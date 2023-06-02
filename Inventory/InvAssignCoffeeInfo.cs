d45c6f2dc8d3505c760d17d4767fccf2fa5aa1fa
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-06-02 22:19:56 +0100

    added a player controller variable to control the inventoryOpen Bool. new TextMesh to control display of coffeeTwo created new method to assign the display information and added the coffeeTwo info.

--- Inventory/InvAssignCoffeeInfo.cs
+++ Inventory/InvAssignCoffeeInfo.cs
@@ -9,24 +9,41 @@ public class InvAssignCoffeeInfo : MonoBehaviour
 {
     public FarmInventory farmInventory;
     public TextMeshProUGUI coffeeOneText;
+    public TextMeshProUGUI coffeeTwoText;
+
+    private PlayerController playerController;
 
     private bool inventoryOpen; // need to assign variable from playercontroller im yet to set up. OnInventory()
     private bool coffeeInfoAssigned; // reset to false when coffee yield is at 0
 
     private void Start()
     {
-        inventoryOpen = true; // just for testing
+        playerController = FindObjectOfType<PlayerController>();
+        //inventoryOpen = true; // just for testing
         
     }
 
     public void Update()
     {
-        if (inventoryOpen) // need to use coffeeInfoAssignedBool
+        inventoryOpen = playerController.inventoryOpen;
+        if (inventoryOpen)
         {
-            coffeeOneText.text = $"Origin: {farmInventory.originCoffeeOne} \n" +
+            DisplayCoffeeInformation();
+        }
+    }
+
+    private void DisplayCoffeeInformation()
+    {
+        coffeeOneText.text = $"Origin: {farmInventory.originCoffeeOne} \n" +
                 $"Variety: {farmInventory.varietalCoffeeOne}\n" +
                 $"Tasting Notes: {farmInventory.tastingNotesCoffeeOne[0]}, {farmInventory.tastingNotesCoffeeOne[1]} \n" +
-                $"Rating: {farmInventory.coffeeRatingCoffeeOne} / 100";
-        }
+                $"Rating: {farmInventory.coffeeRatingCoffeeOne} / 100" +
+                $"Yield: {farmInventory.yieldCoffeeOne} Cups";
+
+        coffeeTwoText.text = $"Origin: {farmInventory.originCoffeeTwo} \n" +
+                $"Variety: {farmInventory.varietalCoffeeTwo}\n" +
+                $"Tasting Notes: {farmInventory.tastingNotesCoffeeTwo[0]}, {farmInventory.tastingNotesCoffeeTwo[1]} \n" +
+                $"Rating: {farmInventory.coffeeRatingCoffeeTwo} / 100" +
+                $"Yield: {farmInventory.yieldCoffeeTwo} Cups";
     }
 }
