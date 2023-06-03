e2c7556d33b65bc95f164cd0b770b99e16d1c055
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-06-03 23:20:01 +0100

    using a coroutine to control the states of the animation when the ufoRB position is at the target position. Still not 100% on how coroutines work and need to research. Just need to create a coffee box gameobject to activate when the ufo is idlelightbeam. Also need to send the ufo back to the start position once the animations have ended.

--- Farm/KennyDGuestCoffee/GuestCoffeeDeliveryKennyD.cs
+++ Farm/KennyDGuestCoffee/GuestCoffeeDeliveryKennyD.cs
@@ -7,34 +7,45 @@ public class GuestCoffeeDeliveryKennyD : MonoBehaviour
 
     private GameObject ufo;
     private Rigidbody2D ufoRB;
+    private Animator ufoAnimator;
 
     public float moveSpeed;
 
     public Vector2 startPosition; // set the start of ufo in inspector for more control
     public Vector2 targetPosition; // set the target position in inspector for more control
 
+    // bool to control ufo movement. might setup movement animations to control with this variable.
     public bool isMoving;
+
+    // creating bools to control animator bools.
+    private bool emitLightBeam;
+    private bool idleLightBeam;
+    private bool exitLightBeam;
+
+
+    public GameObject coffeeBoxToSpawn;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         ufo = gameObject;
-        Debug.Log(ufo);
         ufoRB = ufo.GetComponent<Rigidbody2D>();
+        ufoAnimator = ufo.GetComponent<Animator>();
         ufoRB.position = startPosition;
     }
 
     private void FixedUpdate()
     {
-        
+
         //when player presses button to order guest coffee
 
         //activate game object and move to start position using rigidbody 2D
 
         UfoEnter();
         //move game object from start position to target position using rigidbody 2D
-
+        StartAnimationCycle();
         //when ufo reaches target position start light animation
 
         //when light is at full beam activate present game object and move present
@@ -51,13 +62,52 @@ public class GuestCoffeeDeliveryKennyD : MonoBehaviour
             Vector2 moveDirection = (targetPosition - ufoRB.position).normalized;
             Vector2 movement = moveDirection * (moveSpeed * Time.deltaTime);
             ufoRB.position += movement;
-            
+
             if (Vector2.Distance(ufoRB.position, targetPosition) <= 0.1f)
             {
                 isMoving = false;
                 ufoRB.velocity = Vector2.zero;
+                ufoRB.position = targetPosition;
+                emitLightBeam = true;
             }
         }
+    }
 
+    private IEnumerator CycleThroughUfoAnimations()
+    {
+        if (emitLightBeam)
+        {
+            ufoAnimator.SetBool("EmitLightBeam", true);
+
+            yield return new WaitForSeconds(ufoAnimator.GetCurrentAnimatorStateInfo(0).length);
+
+            emitLightBeam = false;
+            idleLightBeam = true;
+            ufoAnimator.SetBool("EmitLightBeam", false);
+            ufoAnimator.SetBool("IdleLightBeam", true);
+        }
+        if (idleLightBeam)
+        {
+
+            yield return new WaitForSeconds(ufoAnimator.GetCurrentAnimatorStateInfo(0).length);
+
+            idleLightBeam = false;
+            exitLightBeam = true;
+            ufoAnimator.SetBool("IdleLightBeam", false);
+            ufoAnimator.SetBool("ExitLightBeam", true);
+        }
+        if (exitLightBeam)
+        {
+            yield return new WaitForSeconds(ufoAnimator.GetCurrentAnimatorStateInfo(0).length);
+
+            exitLightBeam = false;
+            ufoAnimator.SetBool("ExitLightBeam", false);
+        }
+    }
+
+    private void StartAnimationCycle()
+    {
+        StartCoroutine(CycleThroughUfoAnimations());
     }
 }
+
