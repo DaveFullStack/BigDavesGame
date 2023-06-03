da86cbcf0f445eccda6995aeb2b59723d76ad8a9
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-06-03 22:41:00 +0100

    added a method to move the ufo which is controlled by a bool. I will set the value of the bool using a button on the plantcrops canvas which i will setup later. For now im assigning the bool in the inspector of unity.

--- Farm/KennyDGuestCoffee/GuestCoffeeDeliveryKennyD.cs
+++ Farm/KennyDGuestCoffee/GuestCoffeeDeliveryKennyD.cs
@@ -13,21 +13,26 @@ public class GuestCoffeeDeliveryKennyD : MonoBehaviour
     public Vector2 startPosition; // set the start of ufo in inspector for more control
     public Vector2 targetPosition; // set the target position in inspector for more control
 
+    public bool isMoving;
+
 
     // Start is called before the first frame update
     void Start()
     {
         ufo = gameObject;
+        Debug.Log(ufo);
         ufoRB = ufo.GetComponent<Rigidbody2D>();
         ufoRB.position = startPosition;
     }
 
     private void FixedUpdate()
     {
+        
         //when player presses button to order guest coffee
 
         //activate game object and move to start position using rigidbody 2D
 
+        UfoEnter();
         //move game object from start position to target position using rigidbody 2D
 
         //when ufo reaches target position start light animation
@@ -37,5 +42,22 @@ public class GuestCoffeeDeliveryKennyD : MonoBehaviour
         //when present is at target position light beam turning off animation proceeds.
 
         //when light animation has ended send ufo back to the start position and deactivate
+    }
+
+    private void UfoEnter()
+    {
+        if (isMoving)
+        {
+            Vector2 moveDirection = (targetPosition - ufoRB.position).normalized;
+            Vector2 movement = moveDirection * (moveSpeed * Time.deltaTime);
+            ufoRB.position += movement;
+            
+            if (Vector2.Distance(ufoRB.position, targetPosition) <= 0.1f)
+            {
+                isMoving = false;
+                ufoRB.velocity = Vector2.zero;
+            }
+        }
+
     }
 }
